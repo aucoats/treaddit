59e2e47b766897df22c142934e4e434867290b1e
Poke Code <90790144+aucoats@users.noreply.github.com>
2022-01-22 10:47:29 -0500

    trail model update & trail seeds

--- seeds/index.js
+++ seeds/index.js
@@ -1,11 +1,14 @@
-const seedUsers = require('./user-seeds');
-
 const sequelize = require('../config/connection');
+
+const seedUsers = require('./user-seeds');
+const seedTrails = require('./trail-seeds');
+
 
 const seedAll = async () => {
     await sequelize.sync({ force: true });
     console.log('--------------');
     await seedUsers();
+    await seedTrails();
     process.exit(0);
 };
   
---
+++ seeds/trail-seeds.js
@@ -0,0 +1,34 @@
+const sequelize = require('../config/connection');
+const faker = require('@faker-js/faker');
+const { User, Trail } = require('../models');
+
+const trailData = [
+    {
+        name: `${faker.address.county()}${faker.animal.type()}`,
+        length: `${faker.datatype.number()}`,
+        dog_friendly: `${faker.datatype.boolean()}`,
+        difficulty: faker.helpers.randomize([1, 2, 3, 4, 5]),
+        description: `${faker.lorem.sentences()}`,
+        posted_by: 1,
+    },
+    {
+        name: `${faker.address.county()}${faker.animal.type()}`,
+        length: `${faker.datatype.number()}`,
+        dog_friendly: `${faker.datatype.boolean()}`,
+        difficulty: faker.helpers.randomize([1, 2, 3, 4, 5]),
+        description: `${faker.lorem.sentences()}`,
+        posted_by: 1,
+    },
+    {
+        name: `${faker.address.county()}${faker.animal.type()}`,
+        length: `${faker.datatype.number()}`,
+        dog_friendly: `${faker.datatype.boolean()}`,
+        difficulty: faker.helpers.randomize([1, 2, 3, 4, 5]),
+        description: `${faker.lorem.sentences()}`,
+        posted_by: 1,
+    }
+]
+
+const seedTrails = () => Trail.bulkCreate(trailData);
+
+module.exports = seedTrails; 

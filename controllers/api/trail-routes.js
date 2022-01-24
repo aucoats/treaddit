a7d3a3e00577c51763541afa130043690cd380be
rylee thomas <ryleetreat@gmail.com>
2022-01-24 00:43:03 -0500

    updated search bar and added menu buttons

--- controllers/api/trail-routes.js
+++ controllers/api/trail-routes.js
@@ -1,5 +1,4 @@
 const router = require('express').Router();
-const sequelize = require('../config/connection');
 const { Trail, User } = require('../../models');
 
 router.get('/', (req, res) => {
--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,33 +1,10 @@
 const router = require('express').Router();
-const sequelize = require('../config/connection');
 const { Trail, User } = require('../models');
 
 router.get('/', (req, res) => {
-    Trail.findAll({
-        attributes: [
-            'id',
-            'name',
-            'length',
-            'dog_friendly',
-            'bike_friendly',
-            'difficulty'
-        ], include: [
-            {
-                model: User,
-                attributes: ['username']
-            }
-        ]
-    })
-    .then(dbTrailData => {
-        //pass trails through homepage template
-        const trails = dbTrailData.map(trail => trail.get({plain: true}));
-        res.render('homepage', trails);
-        
-    })
-    .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-    });
-});
+    console.log(req.session);
+
+    res.render('homepage');
+})
 
 module.exports = router; 

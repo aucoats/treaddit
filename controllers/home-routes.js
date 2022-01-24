3b3c10bc1d075fd142ee05f3563c44f0f08636da
rylee thomas <ryleetreat@gmail.com>
2022-01-24 04:13:34 -0500

    updated the public folder/ main.handlebars display

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -4,7 +4,15 @@ const { Trail, User } = require('../models');
 router.get('/', (req, res) => {
     console.log(req.session);
 
-    res.render('homepage');
-})
+    res.render('homepage', {
+        id: 1,
+        name: "California Trail",
+        length: 5,
+        dog_friendly: true,
+        bike_frienldy: false,
+        difficulty: "Moderate",
+        description: "This is a description example.",
+    });
+});
 
 module.exports = router; 

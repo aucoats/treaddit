987505e23672457144b350332b23e0462d5e855f
rylee thomas <ryleetreat@gmail.com>
2022-01-24 18:20:32 -0500

    updated to retrieve all trails and display on homepage

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,10 +1,28 @@
 const router = require('express').Router();
+const sequelize = require('../config/connection');
 const { Trail, User } = require('../models');
 
 router.get('/', (req, res) => {
     console.log(req.session);
 
-    res.render('homepage', {
+    Trail.findAll({
+        attributes: [
+            'id',
+            'name',
+            'dog_friendly',
+            'bike_friendly',
+            'difficulty',
+            'description',
+        ]
+    }) .then(dbTrailData => {
+       const trails = dbTrailData.map(trail => trail.get({ plain: true }));
+       res.render('homepage', {trails});
+    }) .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+
+/*    res.render('homepage', {
         id: 1,
         name: "California Trail",
         length: 5,
@@ -13,6 +31,8 @@ router.get('/', (req, res) => {
         difficulty: "Moderate",
         description: "This is a description example. This is a description example. This is a description example.",
     });
+*/
 });
+
 
 module.exports = router; 

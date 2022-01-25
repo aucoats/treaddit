85bd5f11df74f4b09b76f75f72e360fa240cf676
Poke Code <90790144+aucoats@users.noreply.github.com>
2022-01-24 21:51:31 -0500

    firebase setup and trail updated routes

--- controllers/api/trail-routes.js
+++ controllers/api/trail-routes.js
@@ -1,5 +1,42 @@
 const router = require('express').Router();
+const firebase = require('firebase');
+const multer  = require('multer');
+const upload = multer();
+
+// image prefix to Trail filepath store ** CONCAT WITH SNAPSHOT BELOW ** 
+// gs://treaddit.appspot.com/trails 
+
 const { Trail, User } = require('../../models');
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDyyFmd6Y7okq8KMn7JyROKxfk46gKJfC4",
+    authDomain: "treaddit.firebaseapp.com",
+    projectId: "treaddit",
+    storageBucket: "treaddit.appspot.com",
+    messagingSenderId: "964574079370",
+    appId: "1:964574079370:web:7dd35ffdb6443410a78073",
+    measurementId: "G-60MZQ7M7LG"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+var storage = firebase.storage();
+var storageRef = storage.ref();
+
+const uploadTrailImage = (trailImage) => {
+    var imageRef = storageRef.child(`/trails/${trailImage.originalname}`)
+    var metaData = {
+        contentType: 'image/jpeg'
+    }
+    return imageRef.put(trailImage.buffer, metaData).then((snapshot) => {
+        console.log('Uploaded a blob or file!');
+        console.log(snapshot);
+
+        // snapshot._delegate.metadata.fullPath is to store in Trails DB
+
+
+      });
+}
 
 router.get('/', (req, res) => {
     Trail.findAll({
@@ -19,7 +56,12 @@ router.get('/', (req, res) => {
         //     }
         // ]
     })
-    .then(dbTrailData => res.json(dbTrailData))
+    .then((dbTrailData) => {
+        // retrieve image from DB 
+        // create storage reference
+        // pull image from reference 
+        res.json(dbTrailData)
+    })
     .catch(err => {
         console.log(err);
         res.status(500).json(err);
@@ -60,20 +102,25 @@ router.get('/:id', (req, res) => {
     });
 });
 
-router.post('/', (req, res) => {
-    Trail.create({
-        name: req.body.name,
-        length: req.body.length,
-        dog_friendly: req.body.dog_friendly,
-        bike_friendly: req.body.bike_friendly,
-        difficulty: req.body.difficulty,
-        description: req.body.description
+router.post('/', upload.single("file"), (req, res) => {
+    console.log(req.file);
+    uploadTrailImage(req.file)
+    .then((result) => {
+        res.json(result);
     })
-    .then(dbTrailData => res.json(dbTrailData))
-    .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-    });
+    // Trail.create({
+    //     name: req.body.name,
+    //     length: req.body.length,
+    //     dog_friendly: req.body.dog_friendly,
+    //     bike_friendly: req.body.bike_friendly,
+    //     difficulty: req.body.difficulty,
+    //     description: req.body.description
+    // })
+    // .then(dbTrailData => res.json(dbTrailData))
+    // .catch(err => {
+    //     console.log(err);
+    //     res.status(500).json(err);
+    // });
 });
 
 router.put('/:id', (req, res) => {
--- firebase.js
+++ firebase.js
@@ -1,79 +1,79 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import { getStorage, ref } from "firebase/storage";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+// import { getStorage, ref } from "firebase/storage";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyDyyFmd6Y7okq8KMn7JyROKxfk46gKJfC4",
-  authDomain: "treaddit.firebaseapp.com",
-  projectId: "treaddit",
-  storageBucket: "treaddit.appspot.com",
-  messagingSenderId: "964574079370",
-  appId: "1:964574079370:web:7dd35ffdb6443410a78073",
-  measurementId: "G-60MZQ7M7LG"
-};
+// // Your web app's Firebase configuration
+// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: "AIzaSyDyyFmd6Y7okq8KMn7JyROKxfk46gKJfC4",
+//   authDomain: "treaddit.firebaseapp.com",
+//   projectId: "treaddit",
+//   storageBucket: "treaddit.appspot.com",
+//   messagingSenderId: "964574079370",
+//   appId: "1:964574079370:web:7dd35ffdb6443410a78073",
+//   measurementId: "G-60MZQ7M7LG"
+// };
 
-// Initialize Firebase
-const firebaseApp = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// // Initialize Firebase
+// const firebaseApp = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
 
-// referencces the storage service
-const storage = getStorage(firebaseApp);
+// // referencces the storage service
+// const storage = getStorage(firebaseApp);
 
-// create a storage reference from storage service
-const storageRef = ref(storage);
+// // create a storage reference from storage service
+// const storageRef = ref(storage);
 
-// create a child reference
-const imagesRef = ref(storage, 'images');
-// imageRef now points to 'images'
+// // create a child reference
+// const imagesRef = ref(storage, 'images');
+// // imageRef now points to 'images'
 
-// Child references can also take paths delimited by '/'
-const spaceRef = ref(storage, 'images/space.jpg');
-// spaceRef now points to "images/space.jpg"
-// imagesRef still points to "images"
+// // Child references can also take paths delimited by '/'
+// const spaceRef = ref(storage, 'images/space.jpg');
+// // spaceRef now points to "images/space.jpg"
+// // imagesRef still points to "images"
 
-// Parent allows us to move to the parent of a reference
-const imagesRef = spaceRef.parent;
-// imagesRef now points to 'images'
+// // Parent allows us to move to the parent of a reference
+// const imagesRef = spaceRef.parent;
+// // imagesRef now points to 'images'
 
-// Root allows us to move all the way back to the top of our bucket
-const rootRef = spaceRef.root;
-// rootRef now points to the root
+// // Root allows us to move all the way back to the top of our bucket
+// const rootRef = spaceRef.root;
+// // rootRef now points to the root
 
-// Reference's path is: 'images/space.jpg'
-// This is analogous to a file path on disk
-spaceRef.fullPath;
+// // Reference's path is: 'images/space.jpg'
+// // This is analogous to a file path on disk
+// spaceRef.fullPath;
 
-// Reference's name is the last segment of the full path: 'space.jpg'
-// This is analogous to the file name
-spaceRef.name;
+// // Reference's name is the last segment of the full path: 'space.jpg'
+// // This is analogous to the file name
+// spaceRef.name;
 
-// Reference's bucket is the name of the storage bucket where files are stored
-spaceRef.bucket;
-
-
-// UPLOADING A FILE
-import { getStorage, ref, uploadBytes } from "firebase/storage";
-
-const storage = getStorage();
-const storageRef = ref(storage, 'some-child');
-
-// 'file' comes from the Blob or File API
-uploadBytes(storageRef, file).then((snapshot) => {
-  console.log('Uploaded a blob or file!');
-});
+// // Reference's bucket is the name of the storage bucket where files are stored
+// spaceRef.bucket;
 
 
-// METADATA TO ENSURE ALL FILES ARE UPLOADED AS THE SAME TYPE
-// Create file metadata including the content type
-/** @type {any} */
-const metadata = {
-    contentType: 'image/jpeg',
-  };
+// // UPLOADING A FILE
+// import { getStorage, ref, uploadBytes } from "firebase/storage";
+
+// const storage = getStorage();
+// const storageRef = ref(storage, 'some-child');
+
+// // 'file' comes from the Blob or File API
+// uploadBytes(storageRef, file).then((snapshot) => {
+//   console.log('Uploaded a blob or file!');
+// });
+
+
+// // METADATA TO ENSURE ALL FILES ARE UPLOADED AS THE SAME TYPE
+// // Create file metadata including the content type
+// /** @type {any} */
+// const metadata = {
+//     contentType: 'image/jpeg',
+//   };
   
-  // Upload the file and metadata
-  const uploadTask = uploadBytes(storageRef, file, metadata);
+//   // Upload the file and metadata
+//   const uploadTask = uploadBytes(storageRef, file, metadata);

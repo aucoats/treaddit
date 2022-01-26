ed4eabaf3fa1b7db6bb9c1d05ca1381c3c00a37b
Poke Code <90790144+aucoats@users.noreply.github.com>
2022-01-25 22:04:48 -0500

    front-end-js & firebase merge

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,6 +1,5 @@
 const router = require('express').Router();
 const sequelize = require('../config/connection');
-const Sequelize = require('sequelize');
 const { Trail, User } = require('../models');
 const storageRef = require('./api/trail-routes');
 const downloadTrailImage = require('./api/trail-routes');
@@ -17,57 +16,55 @@ router.get('/', (req, res) => {
             'bike_friendly',
             'difficulty',
             'description',
-            'img_ref'
-        ],
-        // group: 'id',
-        // include: [
-        //     {
-        //         model: Comment,
-        //         attributes: ['id', 'comment_text', 'trail_id', 'user_id', 'created_at'],
-        //         include: {
-        //             model: User,
-        //             attributes: ['username']
-        //         }
-        //     },
-        //     {
-        //         model: Rating,
-        //         attributes: [[Sequelize.fn('AVG', Sequelize.col('rating')), 'avgRating']]
-        //     }
-            // {
-            //     model: User,
-            //     attributes: ['username']
-            // }
-        // ]
-    })
-    .then(dbTrailData =>  {
-        const trails = dbTrailData.map(trail => trail.get({ plain: true }));
-
-        // trails.push(img_src);
-        // console.log('dbTrailData:', dbTrailData)
-        // console.log('img_url:', img_url)
-        // dbTrailData.trail.dataValues.push(img_url);
-        // retrieve image from DB 
-        // create storage reference
-        // pull image from reference 
-        res.render('homepage', {
-            trails,
-        });
-    })
-    // .then(async function imageConvert(dbTrailData) {
-    //     var img_ref = dbTrailData[0].dataValues.img_ref;
-    //     await downloadTrailImage(img_ref).then(response => {
-    //         const img_src = response;
-    //         if (document.querySelector(".img-circle")) {
-    //             var imgElement = document.querySelector(".img-circle");
-    //             imgElement.src=img_url;
-    //         } 
-    //         return img_src;
-    //     })
-    // })
-    .catch(err => {
+        ]
+    }) .then(dbTrailData => {
+       const trails = dbTrailData.map(trail => trail.get({ plain: true }));
+       res.render('homepage', {trails});
+    }) .catch(err => {
         console.log(err);
         res.status(500).json(err);
     });
+
+});
+
+const exphbs = require('express-handlebars');
+const helpers = require('../utils/helpers')
+const hbs = exphbs.create({ helpers });
+
+hbs.handlebars.registerHelper('difficultyLevel', function (difficulty) {
+    if(difficulty == "Easy"){
+        return "success"
+    }
+    if(difficulty == "Moderate"){
+        return "warning"
+    }
+    if(difficulty == "Difficult"){
+        return "danger"
+    }
+});
+
+hbs.handlebars.registerHelper('multiof4', function(id) {
+    var remainder = id % 4;
+    
+    if (id == 1){
+        return true;
+    } else {
+        if (remainder == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+});
+
+hbs.handlebars.registerHelper('multiof3', function(id) {
+    var remainder = id % 3;
+    
+    if(remainder == 0) {
+        return true;
+    } else { 
+        return false;
+    }
 });
 
 module.exports = router; 
--- public/javascript/homePage.js
+++ public/javascript/homePage.js
@@ -20,17 +20,20 @@ loginForm.addEventListener('submit', async (e) => {
 
     //validate success -- need to figure out error handling
     const data = await fetch('api/users/login', requestOptions)
-    .then(response => response.json())
-    .then(data => {
-        console.log(data);
-        return data
-    })
+    .then(response => response)
     .catch(err => console.log(err));
 
-    document.getElementById('login_form').reset()
+    const resolvedData  = await data.json();
+    alert(resolvedData.message);
 
-    // function to update the DOM
-    console.log(data);
+    //error handling
+    if(data.status && data.status !== 200 ) {
+        loginForm.reset();
+    } else {
+    //close modal
+    closeAllModals();
+    window.location.reload();
+    };
 });
 
 
@@ -55,7 +58,7 @@ createUserForm.addEventListener('submit', async (e) => {
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(formData)
     };
-
+    console.log(requestOptions);
     //validate success -- need to figure out error handling
     const data = await fetch('api/users', requestOptions)
     .then(response => response.json())
@@ -67,28 +70,29 @@ createUserForm.addEventListener('submit', async (e) => {
 
     document.getElementById('create_user_form').reset()
 
-    // function to update the DOM
-    console.log(data);
+    //close modal
+    closeAllModals();
+    window.location.reload();
 });
 
 //create trail
 const createTrailForm = document.getElementById('create_trail_form');
 const createTrailName = createTrailForm.querySelector('#trail_name');
 const createTrailLength = createTrailForm.querySelector('#trail_length');
-//attempting to assign const a boolean based on form input. it looks like dog_friendly can be updated on create trail in trail-routes. doesn't seem to update currently
-const createDogFriendly = (createTrailForm.querySelector('#dog_friendly')==='on')?
-true : false;
+const createDogFriendly = createTrailForm.querySelector('#dog_friendly');
+const createBikeFriendly = createTrailForm.querySelector('#bike_friendly');
 const createDifficulty = createTrailForm.querySelector('#difficulty');
 const createDescription = createTrailForm.querySelector('#description');
 
 createTrailForm.addEventListener('submit', async (e) => {
     e.preventDefault();
-   
+
     //logic to get form data
     const formData = {
         name: createTrailName.value,
         length: createTrailLength.value,
-        dog_friendly: createDogFriendly.value,
+        dog_friendly: createDogFriendly.value === 'on',
+        bike_friendly: createBikeFriendly.value === 'on',
         difficulty: createDifficulty.value,
         description: createDescription.value
     }
@@ -99,17 +103,27 @@ createTrailForm.addEventListener('submit', async (e) => {
         body: JSON.stringify(formData)
     };
 
-    //validate success -- need to figure out error handling
+    //validate success, reset form
     const data = await fetch('api/trails', requestOptions)
     .then(response => response.json())
-    .then(data => {
-        console.log(data);
-        return data
-    })
+    
     .catch(err => console.log(err));
 
     document.getElementById('create_trail_form').reset()
 
-    // function to update the DOM
+    //close modal
     console.log(data);
+    closeAllModals();
+    window.location.reload();
 });
+
+//function to close modals
+const loginModal = document.getElementById('loginModal');
+const createTrailModal = document.getElementById('addTrailModal');
+const modals = document.querySelectorAll('.modal');
+
+function closeAllModals () {
+    modals.forEach( (modal) =>{
+        modal.classList.remove('in');
+    })
+};

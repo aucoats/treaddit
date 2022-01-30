614159d48cc5827016c0a5cd62d8d2b018fa8c60
Poke Code <90790144+aucoats@users.noreply.github.com>
2022-01-30 18:43:51 -0500

    some styling, remove rating stars

--- public/javascript/myTrails.js
+++ public/javascript/myTrails.js
@@ -1,4 +1,4 @@
-const { response } = require("express");
+// const { response } = require("express");
 
 //add delete trail api fetch
 const deleteTrail = document.querySelector("#destroyTrailBtn");
